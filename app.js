004f83219b27e65feda81c06238a66fb83a34ee9
JapneetSinghh <japneet8208@gmail.com>
2024-07-20 11:04:13 +0530

    Bug Fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const path = require('path');
 // Setting up the ejs template engine
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serving Public folder As Static For Assets And Css
 app.use(express.static(path.join(__dirname, 'public')));
